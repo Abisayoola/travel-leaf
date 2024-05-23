dde01c66a6e7e7b0c974e11275373652ca6f9282
Abisayo <taiwoabisayo@.com>
2024-05-23 08:39:13 -0700

    landing pages

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
-import React from 'react'
-import './index.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/home/home'
-import About from './pages/about/about'
-import Contact from './pages/contact/contact'
-import Listproperty from './pages/listproperty/listproperty'
-import Booking from './pages/booking/booking'
-import Login from './pages/login/login'
+import React from "react";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/home";
+import About from "./pages/about/about";
+import Listproperty from "./pages/listproperty/listproperty";
+import Booking from "./pages/booking/booking";
+import Login from "./pages/login/login";
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/about' element={<About/>}/>
-      <Route path='/contact' element={<Contact/>}/>
-      <Route path='/listings' element={<Listproperty/>}/>
-      <Route path='/listings:id' element={<Booking/>}/>
-      <Route path='/login' element={<Login/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/listings" element={<Listproperty />} />
+        <Route path="/listings:id" element={<Booking />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </BrowserRouter>
-   
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,22 +1,25 @@
-import React from 'react'
-import './Navbar.css'
-import Logo from '../../assets/logo.png'
+import React from "react";
+import "./Navbar.css";
+import Logo from "../../assets/logo.png";
 
 const Navbar = () => {
   return (
-    <nav className='container'>
-      <img src={Logo} alt='' className='logo'/>
-      <ul className='nav-menu'>
-        <li>Home</li>
-        <li>About</li>
-        <li>List Your Property</li>
-        <li>Contact</li>
-        <ul className='nav-right'>
-        <li>EN</li>
-        <li><button className='btn'>Login</button></li></ul>
+    <nav className="container">
+      <img src={Logo} alt="" className="logo" />
+      <ul className="nav-menu">
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/listings">List Your Property</a>
+        <a href="/contact">Contact</a>
+        <ul className="nav-right">
+          <a>EN</a>
+          <a href="/login">
+            <button className="btn">Login</button>
+          </a>
+        </ul>
       </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
